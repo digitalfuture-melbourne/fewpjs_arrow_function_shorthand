be4d484e3077029ca84f7df7c279f87afc65c78b
Sariputa Tiong <info@digitalfuture.melbourne>
2022-03-01 08:17:38 +1100

    resubmit with arrow function

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function divide() {
     return 2000/100
 }
 
-function square(num1) {
+let square = (num1) => {
     return num1 * num1
 }
 
-function add(num1, num2) {
+let add = (num1, num2) => {
     return num1 + num2
 }
